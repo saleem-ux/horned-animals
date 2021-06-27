077c6c845bf6b255a80d13f6b3321fb0702030e5
saleem-ux <saleem_diab86@yahoo.com>
2021-06-27 16:17:38 -0700

    lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ class Footer  extends React.Component {
+    render() {
+        return (
+            <div>
+              <footer >Author: Saleem Diab</footer>
+            </div>
+        )
+    }
+}
+
+export default Footer
