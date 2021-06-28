d3f52f66a29f8f199de386051bc1abc681c95d74
saleem-ux <saleem_diab86@yahoo.com>
2021-06-28 05:30:47 -0700

    lab01 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
+
 class App extends React.Component {
   render() {
     return (
--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,15 +1,41 @@
-import React from 'react';
+// import React from 'react';
 
+// class HornedBeast extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>UniWhal</h2>
+//                 <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+//                 <p>A unicorn and a narwhal nuzzling their horns</p>
+
+//                 <h2>Unicorn Head</h2>
+//                 <img  src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"  />
+//                 <p>Someone wearing a creepy unicorn head mask</p>
+
+//                 <h2>Rhino Family</h2>
+//                 <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"/>
+//                 <p>Mother (or father) rhino with two babies</p>
+
+
+
+//             </div>
+//         )
+//     }
+// }
+
+// export default HornedBeast
+
+
+import React from 'react'
 class HornedBeast extends React.Component {
     render() {
         return (
             <div>
-                <h2>UniWhal</h2>
-                <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
-                <p>A unicorn and a narwhal nuzzling their horns</p>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image_url} alt={this.props.title} />
+                <p>{this.props.description}</p>
             </div>
         )
     }
 }
-
 export default HornedBeast
